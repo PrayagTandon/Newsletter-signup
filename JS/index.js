65e63dcbc53b412bf270fc28e1365e2cc08a7423
Prayag Tandon <tandonprayag99@gmail.com>
2023-09-06 22:19:16 +0530

    Stuck in Logic

--- JS/index.js
+++ JS/index.js
@@ -4,9 +4,25 @@
 const mainBox = document.querySelector('.main-box');
 const successBox = document.querySelector('.success-box');
 const submitBtn = document.querySelector('.btn-submit');
+const successBtn = document.querySelector('.btn--success');
+
+const emailInputText = document.querySelector('.email-input');
 
 // APPLYING LOGIC
 submitBtn.addEventListener('click', function () {
+
+
+    if (emailInputText === '') {
+        alert('Please enter an E-mail Address!');
+    }
+    else {
+        mainBox.classList.toggle('hidden');
+        successBox.classList.toggle('hidden');
+    }
+
+})
+
+successBtn.addEventListener('click', function () {
     mainBox.classList.toggle('hidden');
     successBox.classList.toggle('hidden');
 })
